31195c60b60221c3fe50783981174e9fa68ab3d0
farukwebcode21 <farukahmadmk@yahoo.com>
2021-09-10 17:13:18 +0600

    es6 more

---
+++ arrow.js
@@ -0,0 +1,19 @@
+// const add =(num1, num2) => num1 + num2;
+// const sum = add(20, 90);
+
+// console.log(sum);
+// function getsum(a, b=9){
+//     return a+ b;
+// }
+// console.log(2, 9);
+
+function functionName(num3, num4){
+   return num3 + num4;
+    // return sum;
+}
+console.log(functionName(20, 20));
+
+const variableName = (num1, num2, num5=7) => num1+ num2 + num5;
+console.log(variableName(20, 20));
+
+const name =['faruk', 'Rakib', 'Sharif', 'Kalam', 'Rohan'];
---
+++ template-string.js
@@ -0,0 +1,8 @@
+const priya = 'Asif Akbar';
+const meye =  "Meya tume ke dukko chiano";
+const kobita = `kobita tume sopno charnni`;
+const multiLine = `This is  multi line 
+how to solve this line do you knonw
+this is salamna khan history
+welcome to our new customer`
+console.log(multiLine);
